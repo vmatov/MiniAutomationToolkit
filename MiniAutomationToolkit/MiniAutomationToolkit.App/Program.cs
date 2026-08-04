edac3210de68d9e6c5bdbfa73d7299291da0c8ed
vmatov <v.matov@evernode.tech>
2026-08-04 19:45:25 +0200

    Задание 3. «Поиск в хаосе»

--- MiniAutomationToolkit/MiniAutomationToolkit.App/Program.cs
+++ MiniAutomationToolkit/MiniAutomationToolkit.App/Program.cs
@@ -1,28 +1,20 @@
 ﻿using MiniAutomationToolkit.Core.Models;
 using MiniAutomationToolkit.Core.Services;
 
-var clientOne = new Client(500, ClientType.Vip);
-var clientTwo = new Client(2000, ClientType.Vip);
-var clientThree = new Client(800, ClientType.Premium);
-var clientFour = new Client(1000, ClientType.Premium);
-var clientFive = new Client(1500, ClientType.Premium);
-var clientSix = new Client(500, ClientType.Regular);
-var clientSeven = new Client(1500, ClientType.Regular);
-var clientEight = new Client(1000, ClientType.Regular);
+// var clientOne = new Client(500, ClientType.Vip);
 
+// DetailPrint(clientOne);
 
-DetailPrint(clientOne);
-DetailPrint(clientTwo);
-DetailPrint(clientThree);
-DetailPrint(clientFour);
-DetailPrint(clientFive);
-DetailPrint(clientSix);
-DetailPrint(clientSeven);
-DetailPrint(clientEight);
+// void DetailPrint(Client client)
+// {
+//     var discount = DiscountCalculator.CalculateDiscount(client.Amount, client.Type);
+//     Console.WriteLine($"Client: {client.Type}, amount: {client.Amount}, discount: {discount}");
+// }
 
-void DetailPrint(Client client)
-{
-    var discount = DiscountCalculator.CalculateDiscount(client.Amount, client.Type);
-    Console.WriteLine($"Client: {client.Type}, amount: {client.Amount}, discount: {discount}");
-}
+var fileNames = new List<string>{"screen_001.log", "error_2026.log", "notes_draft.txt", "screen_002.png", "debug_dump.log", "readme.txt", "screen_screenshot_v2.png", "system_trace.log", "todo_list.txt", "screen_003.png", "access_denied.log", "config_backup.txt", "screen_final.png", "crash_report.log", "instructions.txt", "screen_dashboard.png", "server_response.log", "temp_scratchpad.txt", "screen_preview.png", "network_traffic.log"};
+var fileNamesWithoutScreenshots = new List<string>{"error_2026.log", "notes_draft.txt", "debug_dump.log", "readme.txt", "access_denied.log", "config_backup.txt", "crash_report.log", "instructions.txt", "server_response.log", "temp_scratchpad.txt", "network_traffic.log"};
+var firstScreenshot = FileSearcher.FindFirstScreenshot(fileNames);
+Console.WriteLine($"First screenshot found: {firstScreenshot}");
 
+var secondScreenshot = FileSearcher.FindFirstScreenshot(fileNamesWithoutScreenshots);
+Console.WriteLine($"Second screenshot found: {secondScreenshot}");
