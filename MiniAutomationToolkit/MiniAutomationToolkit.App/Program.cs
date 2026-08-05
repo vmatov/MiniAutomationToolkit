9f42fee82dbfdb7b9d1c464c95f4d1d9625c172d
vmatov <v.matov@evernode.tech>
2026-08-05 09:18:43 +0200

    Задание 3. «Поиск в хаосе» (#3)
    * Задание 2. «Калькулятор скидок»
    
    * Задание 3. «Поиск в хаосе»
    
    * Задание 3 фикс

--- MiniAutomationToolkit/MiniAutomationToolkit.App/Program.cs
+++ MiniAutomationToolkit/MiniAutomationToolkit.App/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("MiniAutomationToolkit.App");
+﻿using MiniAutomationToolkit.Core.Models;
+using MiniAutomationToolkit.Core.Services;
+
+// Задание 2. «Калькулятор скидок»
+
+var clientOne = new Client(500, ClientType.Vip);
+
+DetailPrint(clientOne);
+
+void DetailPrint(Client client)
+{
+    var discount = DiscountCalculator.CalculateDiscount(client.Amount, client.Type);
+    Console.WriteLine($"Client: {client.Type}, amount: {client.Amount}, discount: {discount}");
+}
+
+// Задание 3. «Поиск в хаосе»
+var fileNames = new List<string>{"screen_001.log", "error_2026.log", "notes_draft.txt", "screen_002.png", "debug_dump.log", "readme.txt", "screen_screenshot_v2.png", "system_trace.log", "todo_list.txt", "screen_003.png", "access_denied.log", "config_backup.txt", "screen_final.png", "crash_report.log", "instructions.txt", "screen_dashboard.png", "server_response.log", "temp_scratchpad.txt", "screen_preview.png", "network_traffic.log"};
+var fileNamesWithoutScreenshots = new List<string>{"error_2026.log", "notes_draft.txt", "debug_dump.log", "readme.txt", "access_denied.log", "config_backup.txt", "crash_report.log", "instructions.txt", "server_response.log", "temp_scratchpad.txt", "network_traffic.log"};
+var firstScreenshot = FileSearcher.FindFirstScreenshot(fileNames);
+Console.WriteLine($"First screenshot found: {firstScreenshot}");
+
+// Генерируем ошибку
+// var secondScreenshot = FileSearcher.FindFirstScreenshot(fileNamesWithoutScreenshots);
+// Console.WriteLine($"Second screenshot found: {secondScreenshot}");
---
+++ MiniAutomationToolkit/MiniAutomationToolkit.Core/Helpers/FileSearcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class FileSearcher
+{
+public static string FindFirstScreenshot(List<string> fileNames)
+	{
+		var screenshotFiles = fileNames.FirstOrDefault(fileName => fileName.ToLowerInvariant().EndsWith(".png"));
+		if (screenshotFiles == null)
+		{
+			throw new InvalidOperationException("No screenshots found in the provided list.");
+		}
+		return screenshotFiles;
+	}
+}
