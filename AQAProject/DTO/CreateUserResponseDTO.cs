0f96289f8f4c704939d84d3afe3e801c6780fb99
vmatov <v.matov@evernode.tech>
2026-08-17 21:02:52 +0200

    Домашнее задание к занятию 13 fix

--- AQAProject/DTO/CreateUserResponseDTO.cs
+++ AQAProject/DTO/CreateUserResponseDTO.cs
@@ -15,11 +15,3 @@ public class CreateUserResponseDTO
     public string CreatedAt { get; set; }
     
 }
-
-// {"name":"Stannis","job":"Baratheon Inc.",
-// "id":"923","createdAt":"2026-08-17T18:39:54.730Z",
-// "_meta":{"powered_by":"ReqRes","docs_url":"https://app.reqres.in/documentation",
-// "upgrade_url":"https://app.reqres.in/upgrade","example_url":"https://app.reqres.in/examples/notes-app",
-// "variant":"v1_a","message":"Your data persists here. 
-// Add auth, logs, and custom schemas to build a real backend.",
-// "cta":{"label":"See example app","url":"https://app.reqres.in/examples/notes-app"},"context":"legacy_success"}}
